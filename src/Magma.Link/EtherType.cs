5d939ec74261011155528a62e2b0f3fd1bbc7712
Ben Adams <thundercat@illyriad.co.uk>
2018-05-04 12:58:00 +0100

    Numbers

--- src/Magma.Link/EtherType.cs
+++ src/Magma.Link/EtherType.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Magma.Network
 {
-    public enum ProtocolNumbers:ushort
+    public enum EtherType : ushort
     {
         Arp = 0x0806,
         IPv4 = 0x0800,
---
+++ src/Magma.Network/IPAddress.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Magma.Network
+{
+    public struct IPAddress
+    {
+        private struct V4Address
+        {
+        }
+
+        private struct V6Address
+        {
+        }
+    }
+
+
+}
