aa9fab656234ec50d83a0f34295475487ec3196a
Ben Adams <thundercat@illyriad.co.uk>
2018-05-05 01:35:48 +0100

    IP Address Size

--- src/Magma.Network/IPAddress.cs
+++ src/Magma.Network/IPAddress.cs
@@ -1,17 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+
+using System.Runtime.InteropServices;
 
 namespace Magma.Network
 {
     public struct IPAddress
     {
+        [StructLayout(LayoutKind.Sequential, Pack = 1, Size = 4)]
         public struct V4Address
         {
+            uint _address;
         }
 
+        [StructLayout(LayoutKind.Sequential, Pack = 1, Size = 16)]
         public struct V6Address
         {
+            uint _address0;
+            uint _address1;
+            uint _address2;
+            uint _address3;
         }
     }
 }
