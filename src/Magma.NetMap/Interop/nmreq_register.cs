d6ea0f7c536afccd8f2cd237c72dc20f0f92ead7
Drawaes <seawardtim@gmail.com>
2018-05-05 13:35:39 +0100

    Initial types needed to talk to netmap

---
+++ src/Magma.NetMap/Interop/nmreq_register.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Magma.NetMap
+{
+    internal struct nmreq_register
+    {
+        public ulong nr_offset; /* nifp offset in the shared region */
+        public ulong nr_memsize;    /* size of the shared region */
+        public uint nr_tx_slots;   /* slots in tx rings */
+        public uint nr_rx_slots;   /* slots in rx rings */
+        public ushort nr_tx_rings;   /* number of tx rings */
+        public ushort nr_rx_rings;   /* number of rx rings */
+        public ushort nr_mem_id; /* id of the memory allocator */
+        public ushort nr_ringid; /* ring(s) we care about */
+        public nr_mode nr_mode;   /* specify NR_REG_* modes */
+        public nr_flags nr_flags;  /* additional flags (see below) */
+                                   /* monitors use nr_ringid and nr_mode to select the rings to monitor */
+        public uint nr_extra_bufs; /* number of requested extra buffers */
+    }
+}
