6feb93a72a50cc789efdad3e7d8a437899fd871c
Ben Adams <thundercat@illyriad.co.uk>
2018-05-07 20:51:47 +0100

    Diff ethframe

--- src/Magma.Link/Header/Ethernet.cs
+++ src/Magma.Link/Header/Ethernet.cs
@@ -1,14 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
 using Magma.Link;
 
 namespace Magma.Network.Header
 {
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
     public struct Ethernet
     {
-        MacAddress Destination;
-        MacAddress Source;
-
+        public ulong PreambleSfd;
+        public MacAddress Destination;
+        public MacAddress Source;
+        public EtherType Ethertype;
     }
 }
