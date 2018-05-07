f481f6b1fa9e851cb93cd530957f229cf6786cd8
Drawaes <seawardtim@gmail.com>
2018-05-07 22:58:13 +0100

    experiment

--- src/Magma.NetMap/NetMapHostTxRing.cs
+++ src/Magma.NetMap/NetMapHostTxRing.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using Magma.NetMap.Interop;
+using Magma.Network.Header;
 
 namespace Magma.NetMap
 {
@@ -41,11 +44,35 @@ namespace Magma.NetMap
                     Console.WriteLine("Received data on host ring");
                     var i = RxRingInfo[0].cur;
                     var slot = _rxRing[i];
+                    var buffer = GetBuffer(slot.buf_idx).Slice(0, slot.len);
+                    TryConsume(ref buffer, out var eth);
+
                     _transmitRing.SendWithSwap(_rxRing,(int) i);
                     Console.WriteLine("Passed on host data to a tx ring");
                     RxRingInfo[0].head = RxRingInfo[0].cur = RingNext(i);
                 }
             }
+        }
+
+        public static bool TryConsume(ref Span<byte> span, out Ethernet ethernet)
+        {
+            const int CrcSize = 4;
+
+            if (span.Length >= Unsafe.SizeOf<Ethernet>() + CrcSize)
+            {
+                ethernet = Unsafe.As<byte, Ethernet>(ref MemoryMarshal.GetReference(span));
+                // CRC check
+                span = span.Slice(Unsafe.SizeOf<Ethernet>(), span.Length - (Unsafe.SizeOf<Ethernet>() + CrcSize));
+                Console.WriteLine("There was a crc");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("There was no crc");
+            }
+
+            ethernet = default;
+            return false;
         }
     }
 }
