6ced943bb10434c95096017970f0b433393ea20f
Ben Adams <thundercat@illyriad.co.uk>
2018-05-07 21:21:47 +0100

    refs, endian
    * Diff ethframe
    
    * refs, endian

--- src/Magma.Link/Header/Ethernet.cs
+++ src/Magma.Link/Header/Ethernet.cs
@@ -1,14 +1,21 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Runtime.InteropServices;
 using Magma.Link;
 
 namespace Magma.Network.Header
 {
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
     public struct Ethernet
     {
-        MacAddress Destination;
-        MacAddress Source;
+        public MacAddress Destination;
+        public MacAddress Source;
+        public EtherType Ethertype;
 
+        bool TryConsume(ref Span<byte> span, out Ethernet ethernet)
+        {
+            ethernet = default; // overlay
+            return false; // CRC check, trim span
+        }
     }
 }
