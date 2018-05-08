f9c9e1844a00777c75ee1438c68522140d871af0
Ben Adams <thundercat@illyriad.co.uk>
2018-05-08 03:41:20 +0100

    Add codes

--- src/Magma.Internet.Icmp/Code.cs
+++ src/Magma.Internet.Icmp/Code.cs
@@ -6,8 +6,27 @@ namespace Magma.Internet.Icmp
 {
     public enum Code : ushort
     {
-        EchoReply = ControlMessage.EchoReply << 8 | 0x00,
+        EchoReply =  0x00 << 8 | ControlMessage.EchoReply,
 
-        EchoRequest = ControlMessage.EchoRequest << 8 | 0x00
+        DestinationNetworkUnreachable = 0x00 << 8 | ControlMessage.DestinationUnreachable,
+        DestinationHostUnreachable = 0x01 << 8 | ControlMessage.DestinationUnreachable,
+        DestinationProtocolUnreachable = 0x02 << 8 | ControlMessage.DestinationUnreachable,
+        DestinationPortUnreachable = 0x03 << 8 | ControlMessage.DestinationUnreachable,
+        FragmentationRequired = 0x04 << 8 | ControlMessage.DestinationUnreachable,
+        SourcRouteFailed = 0x05 << 8 | ControlMessage.DestinationUnreachable,
+        DestinationNetworkUnknown = 0x06 << 8 | ControlMessage.DestinationUnreachable,
+        DestinationHostUnknown = 0x07 << 8 | ControlMessage.DestinationUnreachable,
+        SourceHostIsolated = 0x08 << 8 | ControlMessage.DestinationUnreachable,
+        NetworkAdministrativelyProhibited = 0x09 << 8 | ControlMessage.DestinationUnreachable,
+        HostAdministrativelyProhibited = 0x0A << 8 | ControlMessage.DestinationUnreachable,
+        NetworkUnreachableForToS = 0x0B << 8 | ControlMessage.DestinationUnreachable,
+        HostUnreachableForToS = 0x0C << 8 | ControlMessage.DestinationUnreachable,
+        CommunicationAdministrativelyProhibited = 0x0D << 8 | ControlMessage.DestinationUnreachable,
+        HostPrecedenceViolation = 0x0E << 8 | ControlMessage.DestinationUnreachable,
+        PrecedenceCutoffInEffect = 0x0F << 8 | ControlMessage.DestinationUnreachable,
+
+        EchoRequest = 0x00 << 8 | ControlMessage.EchoRequest
+
+        // TODO: Other codes
     }
 }
