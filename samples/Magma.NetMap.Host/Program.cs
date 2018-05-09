376010f159c557c931ee3f71014426dd9f6d3d1a
Ben Adams <thundercat@illyriad.co.uk>
2018-05-09 05:46:00 +0100

    log

--- samples/Magma.NetMap.Host/Program.cs
+++ samples/Magma.NetMap.Host/Program.cs
@@ -122,7 +122,7 @@ namespace Magma.NetMap.Host
             Console.WriteLine($"IP Header length: {Unsafe.SizeOf<IPv4>()}");
             Console.WriteLine($"TCP Header length: {Unsafe.SizeOf<Tcp>()}");
 
-            var netmap = new NetMapPort<PacketReceiver>(interfaceName, transmitter => new PacketReceiver(RingId++, transmitter, logToFile : false));
+            var netmap = new NetMapPort<PacketReceiver>(interfaceName, transmitter => new PacketReceiver(RingId++, transmitter, logToFile : true));
             netmap.Open();
             netmap.PrintPortInfo();
 
