b428604a561b0664684ef396bf5c1e1136c3bb0e
Ben Adams <thundercat@illyriad.co.uk>
2018-05-09 19:55:31 +0100

    Validate IP Checksums (#39)
    * Checksum
    * Checksum recalc
    * Drop invalid checksums

--- src/Magma.Common/Checksum.cs
+++ src/Magma.Common/Checksum.cs
@@ -2,7 +2,62 @@ using System;
 
 namespace Magma.Network
 {
-    public class Checksum
+    public static class Checksum
     {
+        public unsafe static ushort Calcuate<T>(in T buffer, int length)
+            where T : unmanaged
+        {
+            fixed (T* ptr = &buffer)
+            {
+                var pByte = (byte*)ptr;
+
+                long sum = 0;
+
+                var pLong = (long*)ptr;
+                while (length >= sizeof(long))
+                {
+                    var s = *pLong++;
+                    sum += s;
+                    if (sum < s) sum++;
+                    length -= 8;
+                }
+
+                pByte = (byte*)pLong;
+                if ((length & 4) != 0)
+                {
+                    var s = *(uint*)pByte;
+                    sum += s;
+                    if (sum < s) sum++;
+                    pByte += 4;
+                }
+
+                if ((length & 2) != 0)
+                {
+                    var s = *(ushort*)pByte;
+                    sum += s;
+                    if (sum < s) sum++;
+                    pByte += 2;
+                }
+
+                if ((length & 1) != 0)
+                {
+                    var s = *pByte;
+                    sum += s;
+                    if (sum < s) sum++;
+                }
+
+                /* Fold down to 16 bits */
+                var t1 = (uint)sum;
+                var t2 = (uint)(sum >> 32);
+                t1 += t2;
+                if (t1 < t2) t1++;
+                var t3 = (ushort)t1;
+                var t4 = (ushort)(t1 >> 16);
+                t3 += t4;
+                if (t3 < t4) t3++;
+
+                return (ushort)~t3;
+            }
+        }
     }
 }
--- src/Magma.Internet.Icmp/Header/IcmpV4.cs
+++ src/Magma.Internet.Icmp/Header/IcmpV4.cs
@@ -21,7 +21,7 @@ namespace Magma.Network.Header
             set => _typeAndCode = (short)value;
         }
 
-        public short HeaderChecksum;
+        public ushort HeaderChecksum;
         public short Identifier;
         public short SequenceNumber;
         public int Payload;
@@ -39,10 +39,23 @@ namespace Magma.Network.Header
             return false;
         }
 
-        public override string ToString()
+        public unsafe override string ToString()
         {
+            var (isValid, calcChecksum) = ValidateChecksum();
             return "+- Icmp Datagram ----------------------------------------------------------------------+" + Environment.NewLine +
-                  $"| {Type.ToString()} - {Code} | Id: {System.Net.IPAddress.NetworkToHostOrder(Identifier)} | Seq: {System.Net.IPAddress.NetworkToHostOrder(SequenceNumber)} ".PadRight(87) + "|";
+                  $"| {Type.ToString()} - {Code} | Id: {System.Net.IPAddress.NetworkToHostOrder(Identifier)} | Seq: {System.Net.IPAddress.NetworkToHostOrder(SequenceNumber)} ".PadRight(86) +
+                  (isValid ? " " : $"{HeaderChecksum:x}:{calcChecksum:x}")
+                  + "|"
+                  + BitConverter.ToString(new Span<byte>(Unsafe.AsPointer(ref this), Unsafe.SizeOf<IcmpV4>()).ToArray());
+        }
+
+        public (bool isValid, ushort calcChecksum) ValidateChecksum()
+        {
+            var currentChecksum = HeaderChecksum;
+            HeaderChecksum = 0;
+            var newChecksum = Checksum.Calcuate(this, Unsafe.SizeOf<IcmpV4>());
+            HeaderChecksum = currentChecksum;
+            return (currentChecksum == newChecksum, newChecksum);
         }
     }
 }
