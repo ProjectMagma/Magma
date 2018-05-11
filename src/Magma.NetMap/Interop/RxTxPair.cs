380933bfb78b2d12cd3665f180613f6b2756dd8e
Drawaes <seawardtim@gmail.com>
2018-05-11 13:13:49 +0100

    Fix the rxTx issue

--- src/Magma.NetMap/Interop/RxTxPair.cs
+++ src/Magma.NetMap/Interop/RxTxPair.cs
@@ -8,7 +8,7 @@ namespace Magma.NetMap.Interop
     public class RxTxPair
     {
         private FileDescriptor _fileDescriptor;
-        private FileDescriptor _netmapFD;
+        private FileDescriptor _eventFD;
         private EPollHandle _epoll;
         private int _ringId;
         private bool _isHostStack;
@@ -17,9 +17,9 @@ namespace Magma.NetMap.Interop
         {
             _ringId = ringId;
             _isHostStack = isHostStack;
-            _fileDescriptor = CreateEventFD(0, EventFDFlags.EFD_NONBLOCK | EventFDFlags.EFD_SEMAPHORE);
+            _eventFD = CreateEventFD(0, EventFDFlags.EFD_NONBLOCK | EventFDFlags.EFD_SEMAPHORE);
             _epoll = EPollCreate(0);
-            _netmapFD = Open("/dev/netmap", OpenFlags.O_RDWR);
+            _fileDescriptor = Open("/dev/netmap", OpenFlags.O_RDWR);
             if (!_fileDescriptor.IsValid) ExceptionHelper.ThrowInvalidOperation($"Unable to open the /dev/netmap device {_fileDescriptor}");
             var request = new NetMapRequest
             {
