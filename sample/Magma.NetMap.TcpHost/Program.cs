7af644c7d942e75cfd7d1d17f3e8d5b56bd056a8
Drawaes <seawardtim@gmail.com>
2018-05-26 23:36:26 +0100

    Added bind to test project

--- sample/Magma.NetMap.TcpHost/Program.cs
+++ sample/Magma.NetMap.TcpHost/Program.cs
@@ -1,16 +1,18 @@
 using System;
 using System.Net;
+using System.Threading.Tasks;
 
 namespace Magma.NetMap.TcpHost
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var dispatcher = new TestConnectionDispatcher();
             var transport = new NetMapTransport(new IPEndPoint(IPAddress.Any, 6667), "eth0", dispatcher);
-
+            await transport.BindAsync();
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
