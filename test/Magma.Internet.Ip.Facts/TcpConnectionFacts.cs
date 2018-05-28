9b60bf2e7bb5ef4e3fdb415c48ad52771ade2733
Drawaes <seawardtim@gmail.com>
2018-05-28 03:05:23 +0100

    Added the three parts of the handshake

--- test/Magma.Internet.Ip.Facts/TcpConnectionFacts.cs
+++ test/Magma.Internet.Ip.Facts/TcpConnectionFacts.cs
@@ -10,8 +10,10 @@ namespace Magma.Internet.Ip.Facts
 {
     public class TcpConnectionFacts
     {
-        private static readonly byte[] s_synPacket =  "00 19 d2 90 29 61 74 ea 3a 38 f1 00 08 00 45 00 00 3c 00 00 40 00 2d 06 0d 11 d8 12 a6 88 c0 a8 01 68 00 50 c2 c3 29 91 a6 b8 d5 e2 df b5 a0 12 16 a0 f8 e7 00 00 02 04 05 a0 01 01 08 0a 4e 62 b9 10 00 04 aa 62 01 03 03 09".HexToByteArray();
-        
+        private static readonly byte[] s_synPacket = "74 ea 3a 38 f1 00 00 19 d2 90 29 61 08 00 45 00 00 3c 20 e3 40 00 40 06 d9 2d c0 a8 01 68 d8 12 a6 88 c2 c3 00 50 d5 e2 df b4 00 00 00 00 a0 02 20 00 c4 47 00 00 02 04 05 b4 01 03 03 02 04 02 08 0a 00 04 aa 62 00 00 00 00".HexToByteArray();
+        private static readonly byte[] s_synAckPacket =  "00 19 d2 90 29 61 74 ea 3a 38 f1 00 08 00 45 00 00 3c 00 00 40 00 2d 06 0d 11 d8 12 a6 88 c0 a8 01 68 00 50 c2 c3 29 91 a6 b8 d5 e2 df b5 a0 12 16 a0 f8 e7 00 00 02 04 05 a0 01 01 08 0a 4e 62 b9 10 00 04 aa 62 01 03 03 09".HexToByteArray();
+        private static readonly byte[] s_ackPacket = "74 ea 3a 38 f1 00 00 19 d2 90 29 61 08 00 45 00 00 34 20 e4 40 00 40 06 d9 34 c0 a8 01 68 d8 12 a6 88 c2 c3 00 50 d5 e2 df b5 29 91 a6 b9 80 10 10 bc 2a 66 00 00 01 01 08 0a 00 04 aa 81 4e 62 b9 10".HexToByteArray();
+
         [Fact]
         public void ThreeWayHandshakeWorks()
         {
